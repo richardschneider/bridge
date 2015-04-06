bc7167f06a86313039f3c8bd430521335e9e1d9e
Richard Schneider <makaretu@gmail.com>
2015-04-06 12:48:49 +1200

    new release

--- dist/bridge.js
+++ dist/bridge.js
@@ -285,16 +285,30 @@ function Hand() {
 }
 
 /** sort by descending order */
-Hand.prototype.sort = function() {
-    this.cards.sort(function(a,b) {
+Hand.prototype.sort = function () {
+    this.cards.sort(function (a, b) {
         return b.order - a.order;
     });
+    return this;
 };
 
-Hand.prototype.toString = function() {
-    var s = '';
-    for (var i = 0; i < this.cards.length; ++i)
-    {
+Hand.prototype.cardsWithSuit = function (suit) {
+    var cards = [],
+        card,
+        i;
+    for (i = 0; i < this.cards.length; ++i) {
+        card = this.cards[i];
+        if (card.suit === suit) {
+            cards.push(card);
+        }
+    }
+    return cards;
+};
+
+Hand.prototype.toString = function () {
+    var s = '',
+        i;
+    for (i = 0; i < this.cards.length; ++i) {
         if (i !== 0) {
             s += ' ';
         }
