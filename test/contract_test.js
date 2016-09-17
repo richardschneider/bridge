9d7096de918d6b964491ae02e1564ec7876fa6a2
Richard Schneider <makaretu@gmail.com>
2016-09-17 17:42:12 +1200

    feat(contract): add dummy(), leader() and isOpponent()

--- test/contract_test.js
+++ test/contract_test.js
@@ -22,4 +22,34 @@ describe('Contract', function() {
         expect(contract.isPassedIn()).to.equal(false);
     });
 
+    it('should have a dummy', function() {
+        var contract = new bridge.Contract();
+        contract.level = 3;
+        contract.denomination = 'NT';
+        contract.declaror = bridge.seat.south;
+        contract.risk = 'X';
+        expect(contract.dummy()).to.equal(bridge.seat.north);
+    });
+
+    it('should have a leader', function() {
+        var contract = new bridge.Contract();
+        contract.level = 3;
+        contract.denomination = 'NT';
+        contract.declaror = bridge.seat.south;
+        contract.risk = 'X';
+        expect(contract.leader()).to.equal(bridge.seat.west);
+    });
+
+    it('should have opponents', function() {
+        var contract = new bridge.Contract();
+        contract.level = 3;
+        contract.denomination = 'NT';
+        contract.declaror = bridge.seat.south;
+        contract.risk = 'X';
+        expect(contract.isOpponent(bridge.seat.north)).to.equal(false);
+        expect(contract.isOpponent(bridge.seat.south)).to.equal(false);
+        expect(contract.isOpponent(bridge.seat.east)).to.equal(true);
+        expect(contract.isOpponent(bridge.seat.west)).to.equal(true);
+    });
+
 });
