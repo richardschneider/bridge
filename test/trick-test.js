49d2e8fa61eefd18019dec2f5e195f5272b5be26
Richard Schneider <makaretu@gmail.com>
2016-09-30 14:04:33 +1300

    feat(trick): Trick.card(seat) - get the card played by a seat

--- test/trick-test.js
+++ test/trick-test.js
@@ -60,4 +60,24 @@ describe('Trick', function() {
         expect(trick.winner(contract)).to.equal(seat.south);
     });
 
+    it('should return the card played by a seat', function() {
+        var trick = new bridge.Trick();
+        expect(trick.card(seat.north)).to.be.undefined();
+        expect(trick.card(seat.east)).to.be.undefined();
+        expect(trick.card(seat.south)).to.be.undefined();
+        expect(trick.card(seat.west)).to.be.undefined();
+
+        trick.play.push({ seat: seat.west, card: bridge.card.SA } );
+        expect(trick.card(seat.north)).to.be.undefined();
+        expect(trick.card(seat.east)).to.be.undefined();
+        expect(trick.card(seat.south)).to.be.undefined();
+        expect(trick.card(seat.west)).to.equal(bridge.card.SA);
+
+        trick.play.push({ seat: seat.north, card: bridge.card.S2 } );
+        expect(trick.card(seat.north)).to.equal(bridge.card.S2);
+        expect(trick.card(seat.east)).to.be.undefined();
+        expect(trick.card(seat.south)).to.be.undefined();
+        expect(trick.card(seat.west)).to.equal(bridge.card.SA);
+    })
+
 });
